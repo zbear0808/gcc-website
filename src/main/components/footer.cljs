3d661bb1c99c7151ef8cfd4ae0dac294dbc233ea
Zubair Ahmed <zahmed0808@gmail.com>
2025-12-18 19:59:10 -0800

    icons new file and refactor

--- src/main/components/footer.cljs
+++ src/main/components/footer.cljs
@@ -1,25 +1,18 @@
 (ns main.components.footer
   (:require
    [helix.core :refer [defnc $]]
-   [helix.dom :as d]))
+   [helix.dom :as d]
+   [main.components.icons :as icons]))
 
 
-(defnc social-link [{:keys [href label social-class target rel children]}]
-  (d/a {:href href
-        :target (or target "_blank")
-        :rel (or rel "noopener noreferrer")
+(defnc social-link [{:keys [href label social-class on-click children]}]
+  (d/a {:href (when-not on-click href)
+        :target "_blank"
+        :rel "noopener noreferrer"
         :class (str "btn social-link " social-class)
-        :aria-label label}
-       (d/svg {:xmlns "http://www.w3.org/2000/svg"
-               :width "24"
-               :height "24"
-               :viewBox "0 0 24 24"
-               :fill "none"
-               :stroke "currentColor"
-               :stroke-width "2"
-               :stroke-linecap "round"
-               :stroke-linejoin "round"}
-              children)))
+        :aria-label label
+        :on-click on-click}
+       children))
 
 
 (defnc footer []
@@ -31,34 +24,31 @@
        {:href "https://instagram.com/zugood"
         :label "Instagram"
         :social-class "social-link-instagram"}
-       (d/rect {:x "2" :y "2" :width "20" :height "20" :rx "5" :ry "5"})
-       (d/path {:d "M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"})
-       (d/line {:x1 "17.5" :y1 "6.5" :x2 "17.51" :y2 "6.5"}))
+       ($ icons/instagram-icon))
 
     ($ social-link
        {:href "https://linkedin.com/in/zubaira2"
         :label "LinkedIn"
         :social-class "social-link-linkedin"}
-       (d/path {:d "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"})
-       (d/rect {:x "2" :y "9" :width "4" :height "12"})
-       (d/circle {:cx "4" :cy "4" :r "2"}))
+       ($ icons/linkedin-icon))
 
     ($ social-link
        {:href "https://github.com/zbear0808"
         :label "GitHub"
         :social-class "social-link-github"}
-       (d/path {:d "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"}))
+       ($ icons/github-icon))
 
     ($ social-link
        {:href "#"
         :label "YouTube"
         :social-class "social-link-youtube"}
-       (d/path {:d "M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"})
-       (d/polygon {:points "9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"}))
+       ($ icons/youtube-icon))
 
     ($ social-link
        {:href "mailto:zugood.lasers@gmail.com"
         :label "Email"
-        :social-class "social-link-email"}
-       (d/path {:d "M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"})
-       (d/polyline {:points "22,6 12,13 2,6"})))))
+        :social-class "social-link-email"
+        :on-click (fn [e]
+                    (.preventDefault e)
+                    (.open js/window "mailto:zugood.lasers@gmail.com" "mail"))}
+       ($ icons/email-icon)))))
