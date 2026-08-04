dbb0d5c6154ab1b425c708462e07309bfed0202a
Zubair Ahmed <zahmed0808@gmail.com>
2026-08-04 14:26:55 -0700

    render phob and notches

--- src/components/ControllerVisualizer.tsx
+++ src/components/ControllerVisualizer.tsx
@@ -26,59 +26,113 @@ const ControllerVisualizer: React.FC = () => {
     isParacord = true;
   }
 
+  const isDIY = config.product === 'diy-kit' || config.product === '0-solder-diy-kit';
+
   return (
     <div className="visualizer-wrapper">
       <div className={`controller-map ${config.buttons ? `theme-${config.buttons}` : ''}`}>
-        {/* Left Trigger */}
-        <div className={`trigger trigger-l ${leftTriggerClass}`}></div>
-        {/* Right Trigger */}
-        <div className={`trigger trigger-r ${rightTriggerClass}`}></div>
+        {isDIY ? (
+          <div className="phob-pcb">
+            {config.cable && (
+              <div className="cord-container pcb-cord">
+                <div className={`cord ${isParacord ? 'paracord' : ''}`}></div>
+                <div className="plug" style={{ backgroundColor: plugColor }}>
+                  <div className="plug-details"></div>
+                </div>
+              </div>
+            )}
+            <div className="pcb-board">
+              <svg className="pcb-svg" width="280" height="200" viewBox="0 0 280 200">
+                <defs>
+                  <filter id="pcb-shadow" x="-10%" y="-10%" width="120%" height="120%">
+                    <feDropShadow dx="0" dy="4" stdDeviation="4" floodOpacity="0.15" />
+                  </filter>
+                </defs>
+                <path 
+                  d="M 50 10 L 230 10 A 40 40 0 0 1 270 50 L 270 130 A 50 50 0 0 1 170 130 L 170 110 A 30 30 0 0 0 110 110 L 110 140 A 50 50 0 0 1 10 140 L 10 50 A 40 40 0 0 1 50 10 Z" 
+                  fill="#f0f0f0" 
+                  stroke="#ddd"
+                  strokeWidth="2"
+                  filter="url(#pcb-shadow)"
+                />
+              </svg>
+              
+              <div className="pcb-chip"></div>
+              <div className="pcb-chip-small"></div>
+              <div className="pcb-start-pad"></div>
+              
+              <div className="pcb-stick-box-left"></div>
+              
+              <div className="pcb-ribbon-cable"></div>
+              <div className="pcb-daughterboard">
+                <div className="pcb-stick-box-c"></div>
+              </div>
+              
+              <div className="pcb-button-pad pad-a"></div>
+              <div className="pcb-button-pad pad-b"></div>
+              <div className="pcb-button-pad pad-x"></div>
+              <div className="pcb-button-pad pad-y"></div>
+              
+              <div className="pcb-button-pad pad-dup"></div>
+              <div className="pcb-button-pad pad-ddown"></div>
+              <div className="pcb-button-pad pad-dleft"></div>
+              <div className="pcb-button-pad pad-dright"></div>
+            </div>
+          </div>
+        ) : (
+          <>
+            {/* Left Trigger */}
+            <div className={`trigger trigger-l ${leftTriggerClass}`}></div>
+            {/* Right Trigger */}
+            <div className={`trigger trigger-r ${rightTriggerClass}`}></div>
 
-        <div 
-          className="controller-body" 
-          style={{ backgroundColor: shellColor }}
-        >
-          {/* Cord */}
-          {config.cable && (
-            <div className="cord-container">
-              <div className={`cord ${isParacord ? 'paracord' : ''}`}></div>
-              <div className="plug" style={{ backgroundColor: plugColor }}>
-                <div className="plug-details"></div>
+            <div 
+              className="controller-body" 
+              style={{ backgroundColor: shellColor }}
+            >
+              {/* Cord */}
+              {config.cable && (
+                <div className="cord-container">
+                  <div className={`cord ${isParacord ? 'paracord' : ''}`}></div>
+                  <div className="plug" style={{ backgroundColor: plugColor }}>
+                    <div className="plug-details"></div>
+                  </div>
+                </div>
+              )}
+
+              {/* Left Pod */}
+              <div className="controller-pod-left" style={{ backgroundColor: shellColor }}></div>
+              
+              {/* Right Pod */}
+              <div className="controller-pod-right" style={{ backgroundColor: shellColor }}></div>
+
+              {/* Left Handle */}
+              <div className="controller-handle-left" style={{ backgroundColor: shellColor }}></div>
+              
+              {/* Right Handle */}
+              <div className="controller-handle-right" style={{ backgroundColor: shellColor }}></div>
+              
+              {/* D-Pad */}
+              <div className="d-pad"></div>
+              
+              {/* Left Stick */}
+              <div className={`stick stick-left ${hasFirefox ? 'has-firefox' : ''} ${hasWavedash ? 'has-wavedash' : ''} notch-style-${config.notchStyle || 'deep'}`}>
+                <div className="notch-indicator"></div>
+              </div>
+              
+              {/* C-Stick (Yellow) */}
+              <div className="stick stick-c"></div>
+              
+              {/* Button Cluster */}
+              <div className="button-group">
+                <div className="btn-v btn-a">A</div>
+                <div className="btn-v btn-b">B</div>
+                <div className="btn-v btn-x">X</div>
+                <div className="btn-v btn-y">Y</div>
               </div>
             </div>
-          )}
-
-          {/* Left Pod */}
-          <div className="controller-pod-left" style={{ backgroundColor: shellColor }}></div>
-          
-          {/* Right Pod */}
-          <div className="controller-pod-right" style={{ backgroundColor: shellColor }}></div>
-
-          {/* Left Handle */}
-          <div className="controller-handle-left" style={{ backgroundColor: shellColor }}></div>
-          
-          {/* Right Handle */}
-          <div className="controller-handle-right" style={{ backgroundColor: shellColor }}></div>
-          
-          {/* D-Pad */}
-          <div className="d-pad"></div>
-          
-          {/* Left Stick */}
-          <div className={`stick stick-left ${hasFirefox ? 'has-firefox' : ''} ${hasWavedash ? 'has-wavedash' : ''} notch-style-${config.notchStyle || 'deep'}`}>
-            <div className="notch-indicator"></div>
-          </div>
-          
-          {/* C-Stick (Yellow) */}
-          <div className="stick stick-c"></div>
-          
-          {/* Button Cluster */}
-          <div className="button-group">
-            <div className="btn-v btn-a">A</div>
-            <div className="btn-v btn-b">B</div>
-            <div className="btn-v btn-x">X</div>
-            <div className="btn-v btn-y">Y</div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
